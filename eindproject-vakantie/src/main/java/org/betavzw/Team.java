2462349b23a67f617bef682fdb36be160f2b62f8
Koen De Voegt <koen@devoegt.be>
2015-09-10 16:03:38 +0200

    Added Team

---
+++ eindproject-vakantie/src/main/java/org/betavzw/Team.java
@@ -0,0 +1,23 @@
+package org.betavzw;
+
+public class Team {
+	private String naam;
+	private String code;
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
