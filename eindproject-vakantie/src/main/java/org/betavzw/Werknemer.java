67b16a8157aa3b3b661790e70d171f725a8e2cb1
Koen De Voegt <koen@devoegt.be>
2015-09-10 15:58:42 +0200

    Werknemer basic getters and setters

--- eindproject-vakantie/src/main/java/org/betavzw/Werknemer.java
+++ eindproject-vakantie/src/main/java/org/betavzw/Werknemer.java
@@ -1,5 +1,89 @@
 package org.betavzw;
 
+import java.time.LocalDate;
+
 public class Werknemer {
+
+	private String naam;
+	private String voornaam;
+	private String email;
+	private LocalDate geboortedatum;
+	private int personeelsnr;
+	private String adres;
+	private int postcode;
+	private String gemeente;
+	private String paswoord;
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public void setVoornaam(String voornaam) {
+		this.voornaam = voornaam;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getGeboortedatum() {
+		return geboortedatum;
+	}
+
+	public void setGeboortedatum(LocalDate geboortedatum) {
+		this.geboortedatum = geboortedatum;
+	}
+
+	public int getPersoneelsnr() {
+		return personeelsnr;
+	}
+
+	public void setPersoneelsnr(int personeelsnr) {
+		this.personeelsnr = personeelsnr;
+	}
+
+	public String getAdres() {
+		return adres;
+	}
+
+	public void setAdres(String adres) {
+		this.adres = adres;
+	}
+
+	public int getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(int postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getGemeente() {
+		return gemeente;
+	}
+
+	public void setGemeente(String gemeente) {
+		this.gemeente = gemeente;
+	}
+
+	public String getPaswoord() {
+		return paswoord;
+	}
+
+	public void setPaswoord(String paswoord) {
+		this.paswoord = paswoord;
+	}
 
 }
