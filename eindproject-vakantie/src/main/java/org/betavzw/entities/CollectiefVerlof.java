d2233a2a5a43e9cdbf069728b06edd75cef37be9
Koen De Voegt <koen@devoegt.be>
2015-09-10 16:30:43 +0200

    Added CollectiefVerlof

---
+++ eindproject-vakantie/src/main/java/org/betavzw/entities/CollectiefVerlof.java
@@ -0,0 +1,15 @@
+package org.betavzw.entities;
+
+import java.time.LocalDate;
+
+public class CollectiefVerlof extends CollectieveSluiting {
+	private LocalDate eindDatum;
+
+	public LocalDate getEindDatum() {
+		return eindDatum;
+	}
+
+	public void setEindDatum(LocalDate eindDatum) {
+		this.eindDatum = eindDatum;
+	}
+}
