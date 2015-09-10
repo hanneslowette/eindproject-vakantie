d2dd92815a39621a2cef80c8e40326f415c45fb7
Koen De Voegt <koen@devoegt.be>
2015-09-10 16:21:28 +0200

    Added JaarlijksVerlof

---
+++ eindproject-vakantie/src/main/java/org/betavzw/ejbs/JaarlijksVerlof.java
@@ -0,0 +1,22 @@
+package org.betavzw.ejbs;
+
+public class JaarlijksVerlof {
+	private int jaar;
+	private int aantalDagen;
+
+	public int getJaar() {
+		return jaar;
+	}
+
+	public void setJaar(int jaar) {
+		this.jaar = jaar;
+	}
+
+	public int getAantalDagen() {
+		return aantalDagen;
+	}
+
+	public void setAantalDagen(int aantalDagen) {
+		this.aantalDagen = aantalDagen;
+	}
+}
