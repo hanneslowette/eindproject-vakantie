e89fc5c28b8709a054bd10ae60031a91cf9b4067
Koen De Voegt <koen@devoegt.be>
2015-09-10 16:57:50 +0200

    Refactored Status -> Toestand

--- eindproject-vakantie/src/main/java/org/betavzw/entities/Team.java
+++ eindproject-vakantie/src/main/java/org/betavzw/entities/Team.java
@@ -1,13 +1,25 @@
 package org.betavzw.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Team {
 	
 	private String naam;
 	private String code;
 	private Werknemer teamverantwoordelijke;
+	private Set<Werknemer> teamLeden = new HashSet<Werknemer>();
 
 	public String getNaam() {
 		return naam;
+	}
+
+	public Set<Werknemer> getTeamLeden() {
+		return teamLeden;
+	}
+
+	public void setTeamLeden(Set<Werknemer> teamLeden) {
+		this.teamLeden = teamLeden;
 	}
 
 	public void setNaam(String naam) {
@@ -30,6 +42,8 @@ public class Team {
 		this.teamverantwoordelijke = teamverantwoordelijke;
 	}
 
-	
+	public void addWerknemer(Werknemer teamlid) {
+		
+	}
 	
 }
