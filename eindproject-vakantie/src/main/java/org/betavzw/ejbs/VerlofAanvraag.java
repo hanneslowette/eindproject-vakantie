fbcb8e369383d792e5b3dad798318aec5c993dde
Koen De Voegt <koen@devoegt.be>
2015-09-10 16:19:17 +0200

    Added VerlofAanvraag

--- eindproject-vakantie/src/main/java/org/betavzw/ejbs/VerlofAanvraag.java
+++ eindproject-vakantie/src/main/java/org/betavzw/ejbs/VerlofAanvraag.java
@@ -1,5 +1,52 @@
 package org.betavzw.ejbs;
 
+import java.time.LocalDate;
+
 public class VerlofAanvraag {
+	private LocalDate startDatum;
+	private LocalDate eindDatum;
+	private LocalDate aanvraagDatum;
+	private int toestand;
+
+	public LocalDate getStartDatum() {
+		return startDatum;
+	}
+
+	public void setStartDatum(LocalDate startDatum) {
+		this.startDatum = startDatum;
+	}
+
+	public LocalDate getEindDatum() {
+		return eindDatum;
+	}
+
+	public void setEindDatum(LocalDate eindDatum) {
+		this.eindDatum = eindDatum;
+	}
+
+	public LocalDate getAanvraagDatum() {
+		return aanvraagDatum;
+	}
+
+	public void setAanvraagDatum(LocalDate aanvraagDatum) {
+		this.aanvraagDatum = aanvraagDatum;
+	}
+
+	public int getToestand() {
+		return toestand;
+	}
+
+	public void setToestand(int toestand) {
+		this.toestand = toestand;
+	}
+
+	public void Goedkeuren() {
+	}
+
+	public void Afkeuren() {
+	}
+
+	public void Annuleren() {
+	}
 
 }
