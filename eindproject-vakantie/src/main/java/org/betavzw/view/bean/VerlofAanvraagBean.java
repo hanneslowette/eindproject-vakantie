562816d58d674eb6abb1e652c2e2d6c890b00527
user109 <user109@192.168.23.123>
2015-09-11 11:01:17 +0200

    verdergewerkt aan view en bean verlofaanvraag.

--- eindproject-vakantie/src/main/java/org/betavzw/view/bean/VerlofAanvraagBean.java
+++ eindproject-vakantie/src/main/java/org/betavzw/view/bean/VerlofAanvraagBean.java
@@ -1,5 +1,7 @@
 package org.betavzw.view.bean;
 
+import java.util.Date;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -11,9 +13,37 @@ import org.betavzw.entities.VerlofAanvraag;
 public class VerlofAanvraagBean {
 
 	@EJB
-	private VerlofAanvraag verlofAanvraag;	
-	
+	private VerlofAanvraag verlofAanvraag;
+	private Date startDatum;
+	private Date eindDatum;
+
+	public VerlofAanvraag getVerlofAanvraag() {
+		return verlofAanvraag;
+	}
+
+	public void setVerlofAanvraag(VerlofAanvraag verlofAanvraag) {
+		this.verlofAanvraag = verlofAanvraag;
+	}
+
+	public Date getStartDatum() {
+		return startDatum;
+	}
+
+	public void setStartDatum(Date startDatum) {
+		this.startDatum = startDatum;
+	}
+
+	public Date getEindDatum() {
+		return eindDatum;
+	}
+
+	public void setEindDatum(Date eindDatum) {
+		if (startDatum.before(eindDatum)) {
+			this.eindDatum = eindDatum;
+		}
+	}
+
 	public String verstuur() {
-		return "verlofaanvraag.xhtml";
+		return "home.xhtml";
 	}
 }
