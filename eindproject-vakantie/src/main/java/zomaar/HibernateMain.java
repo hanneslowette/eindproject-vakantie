206fd25b76178235a98be7ee1b395cb3eeff813d
user106 <user106@192.168.23.115>
2015-09-11 13:19:55 +0200

    dcdcdc

--- eindproject-vakantie/src/main/java/zomaar/HibernateMain.java
+++ eindproject-vakantie/src/main/java/zomaar/HibernateMain.java
@@ -2,7 +2,6 @@ package zomaar;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import org.betavzw.entities.Werknemer;
