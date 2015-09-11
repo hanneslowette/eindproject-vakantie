3402e9eabce6d2dc43237a36bccaace469b0fc70
user109 <user109@192.168.23.123>
2015-09-11 10:35:03 +0200

    .

--- eindproject-vakantie/src/main/java/org/betavzw/view/bean/VerlofAanvraagBean.java
+++ eindproject-vakantie/src/main/java/org/betavzw/view/bean/VerlofAanvraagBean.java
@@ -1,5 +1,19 @@
 package org.betavzw.view.bean;
 
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import org.betavzw.entities.VerlofAanvraag;
+
+@Named("verlofAanvraag")
+@SessionScoped
 public class VerlofAanvraagBean {
 
+	@EJB
+	private VerlofAanvraag verlofAanvraag;	
+	
+	public String verstuur() {
+		return "verlofaanvraag.xhtml";
+	}
 }
