3b8f4dd8be77c646057c8d05b2dfe2c5120a9d2c
Koen De Voegt <koen@devoegt.be>
2015-09-11 10:18:14 +0200

    Added HibernateMain

---
+++ eindproject-vakantie/src/main/java/zomaar/HibernatemMain.java
@@ -0,0 +1,22 @@
+package zomaar;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernatemMain {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("OefQueriesPU");
+		
+		EntityManager em = emf.createEntityManager();
+
+		
+		em.close();
+		emf.close();
+		
+	}
+
+}
