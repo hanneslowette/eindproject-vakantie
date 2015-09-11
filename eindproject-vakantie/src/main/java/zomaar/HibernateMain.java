ed0245489b42ea84eee62ac45b657c2570cbdc75
Koen De Voegt <koen@devoegt.be>
2015-09-11 10:51:05 +0200

    update

--- eindproject-vakantie/src/main/java/zomaar/HibernateMain.java
+++ eindproject-vakantie/src/main/java/zomaar/HibernateMain.java
@@ -20,9 +20,15 @@ public class HibernateMain {
 		tx.begin();
 		
 		Werknemer koen = new Werknemer();
-		koen.setNaam("Voornaam");
-		koen.setVoornaam("Koen");
+		koen.setNaam("Koen");
+		koen.setVoornaam("De Voegt");
 		
+		Werknemer brent = new Werknemer();
+		koen.setNaam("Courtois");
+		koen.setVoornaam("Brent");
+		
+		em.persist(koen);
+		em.persist(brent);
 		
 		em.close();
 		emf.close();
