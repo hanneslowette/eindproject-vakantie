bae8501261e171bf989751604cc080c2b83f58ce
Koen De Voegt <koen@devoegt.be>
2015-09-11 10:18:40 +0200

    Renamed HibernateMain

--- eindproject-vakantie/src/main/java/zomaar/HibernateMain.java
+++ eindproject-vakantie/src/main/java/zomaar/HibernateMain.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class HibernatemMain {
+public class HibernateMain {
 
 	public static void main(String[] args) {
 		
