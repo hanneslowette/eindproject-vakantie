8e53b1e4f92029416a818562b69087476ca77473
user109 <user109@192.168.23.123>
2015-09-11 13:11:12 +0200

    aanmaak VerlofAanvraagEJB

---
+++ eindproject-vakantie/src/main/java/org/betavzw/ejb/VerlofAanvraagEJB.java
@@ -0,0 +1,18 @@
+package org.betavzw.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class VerlofAanvraagEJB
+ */
+@Stateless
+public class VerlofAanvraagEJB implements VerlofAanvraagEJBLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public VerlofAanvraagEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
